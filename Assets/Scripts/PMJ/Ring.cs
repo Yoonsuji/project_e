0c0a598bf058212000c315eaeb373164b456eaae
tigerspower <pmj0316985@gmail.com>
2024-01-03 14:37:28 +0900

    ring scripts

--- Assets/Scripts/PMJ/Ring.cs
+++ Assets/Scripts/PMJ/Ring.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
+    public bool right;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Ring : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)) { }   
     }
+
+
 }
