d408c2dcdb77f6d220b83066426ad9943b98ce8a
Suji <103244178+Yoonsuji@users.noreply.github.com>
2024-01-08 14:53:35 +0900

    fix:Hand, time

--- Assets/Script/YSJ/HandController.cs
+++ Assets/Script/YSJ/HandController.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HandController : MonoBehaviour
 {
@@ -10,52 +7,46 @@ public class HandController : MonoBehaviour
     public Transform HandTarget2;
     public float Speed = 1.0f;
     public bool isMoving = false;
-
     private Coroutine currentBehavior;
+
+
     void Update()
     {
-        if (!isMoving) 
+        if (!isMoving && currentBehavior == null)
         {
-            StartCoroutine(Move());
+            StartHandMove();
         }
     }
+
     public void StartHandMove()
     {
         if (currentBehavior != null)
         {
             StopCoroutine(currentBehavior);
+            gameObject.SetActive(false);
         }
         currentBehavior = StartCoroutine(HandMove());
     }
 
-
-    public IEnumerator Move()
+    IEnumerator HandMove()
     {
         isMoving = true;
         float elapsedTime = 0f;
         Vector3 startingPos = transform.position;
 
-        while(elapsedTime < 1f)
-        {
-            transform.position = Vector3.Lerp(startingPos, HandTarget.position, elapsedTime);
-            elapsedTime += Time.deltaTime * Speed;
-
-            yield return null;
-        }
-        isMoving = false;
-    }
-
-    IEnumerator HandMove()
-    {
-        float elapsedTime = 0f;
-        Vector3 startingPos = transform.position;
         while (elapsedTime < 1f)
         {
-            transform.position = Vector3.Lerp(startingPos, HandTarget2.position, elapsedTime);
+            transform.position = Vector3.LerpUnclamped(startingPos, HandTarget2.position, elapsedTime);
             elapsedTime += Time.deltaTime * Speed;
 
             yield return null;
         }
+
+        Vector3 temp = HandTarget.position;
+        HandTarget.position = HandTarget2.position;
+        HandTarget2.position = temp;
+
+        isMoving = false;
         currentBehavior = null;
     }
 }
