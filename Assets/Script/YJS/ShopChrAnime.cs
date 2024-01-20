0f3d1d1fa4d59fbb014b79126b9905b8482c2b50
yoonjiseang <imzzang3u@naver.com>
2024-01-20 17:14:11 +0900

    메인, 상점 신 캐릭터 애니

--- Assets/Script/YJS/ShopChrAnime.cs
+++ Assets/Script/YJS/ShopChrAnime.cs
@@ -6,15 +6,19 @@ using UnityEngine.Animations;
 public class ShopChrAnime : MonoBehaviour
 {
     public CapybaraCurrentItem CapybaraCurrentItem;
+    public List<RuntimeAnimatorController> Animations = new List<RuntimeAnimatorController>();
     private void Update()
     {
         if (CapybaraCurrentItem.currentCloth != null)
         {
-            this.GetComponent<Animator>().SetInteger("Skin", CapybaraCurrentItem.currentCloth.ClothNum);
+            //this.GetComponent<Animator>().SetInteger("Skin", CapybaraCurrentItem.currentCloth.ClothNum);
+            this.GetComponent<Animator>().runtimeAnimatorController = Animations[CapybaraCurrentItem.currentCloth.ClothNum];
         }
         else
         {
-            this.GetComponent<Animator>().SetInteger("Skin", 0);
+            this.GetComponent<Animator>().runtimeAnimatorController = Animations[0];
+
+            //this.GetComponent<Animator>().SetInteger("Skin", 0);
         }
     }
 }
